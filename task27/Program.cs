f89761607b950d07b534cf4c5c862b2a9adae876
Nariman324 <119915847+Nariman324@users.noreply.github.com>
2022-12-12 20:00:46 +0600

    Add files via upload

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int CountNum(int num)
+{
+    int sum = 0;
+    while(num>0)
+    {
+        int x = num%10;
+        num /=10;
+        sum +=x;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число N");
+int n=int.Parse(Console.ReadLine()!);
+Console.Write($"Сумма цифр в числе {n}={CountNum(n)}");
